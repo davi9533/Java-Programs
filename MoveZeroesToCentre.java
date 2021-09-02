d55bb345e070e976c9165d2d25bce0a19a57e0d7
davi9533 <38536291+davi9533@users.noreply.github.com>
2021-09-01 20:05:44 -0400

    Add files via upload

---
+++ MoveZeroesToCentre.java
@@ -0,0 +1,46 @@
+package array;
+
+public class MoveZeroesToCentre {
+	
+	public static void move(int arr[]) {
+		
+		int mid = (int) Math.floor((arr.length)/2);
+		int count = 0;
+		int last = arr.length-1;
+		//first half
+		
+		for(int i=0;i<mid;i++) {
+			
+			if(arr[i]!=0) {
+				arr[count++] =arr[i];
+			}
+		}
+		while(count<mid) {
+			arr[count++]=0;
+		}
+		
+		//second half
+		
+		for(int i=arr.length-1;i>=mid;i--) {
+			if(arr[i]!=0) {
+				arr[last--]=arr[i];
+			}
+		}
+		while(last>=mid) {
+			arr[last--]=0;
+		}
+		
+		
+		//print
+		for(int i=0;i<arr.length;i++)
+			System.out.print("  " + arr[i]);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {0,1,0,2,0,4,0,5,0};
+		move(arr);
+	}
+
+}
